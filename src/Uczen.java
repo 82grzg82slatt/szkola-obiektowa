7387981a50d46d5f6bb934ad65ee9d0d7dd9a48b
82grzg82slatt <pizzapan65@gmail.com>
2024-09-19 12:11:38 +0200

    kopiowane

---
+++ src/Uczen.java
@@ -0,0 +1,33 @@
+public class Uczen extends Osoba implements Dyzurny{
+    /*
+    Uczen to klasa potomna
+    Osoba to klasa bazowa
+    Uczen dziedziczy wszystko? po klasie Osoba
+     */
+    private int nrEwidencyjny;
+    static int liczbaObiektow;
+
+    public Uczen(String imie, String nazwisko, int wiek) {
+        super(imie, nazwisko, wiek);//wywołujemy konstruktor klasy bazowej
+        liczbaObiektow++;
+        nrEwidencyjny = liczbaObiektow;
+    }
+
+    public Uczen(String imie, String nazwisko, int wiek,int nrEwidencyjny) {
+        super(imie, nazwisko,wiek);
+        this.nrEwidencyjny = nrEwidencyjny;
+        liczbaObiektow++;
+    }
+
+    @Override
+    public String toString() {
+        return "Uczen imie:"+ getImie()+" nazwisko: "+nazwisko +
+                "nrEwidencyjny=" + nrEwidencyjny +
+                "} " ;
+    }
+
+    @Override
+    public void wykonajDyzur() {
+        System.out.println("Wytarłem tablicę");
+    }
+}
